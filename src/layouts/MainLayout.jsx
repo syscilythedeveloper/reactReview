046fbdeccfacc9adce4e6680a28dcbe49d4516e2
syscilythedeveloper <sbrown@syscilythedeveloper.com>
2024-07-03 20:15:42 -0400

    react refresher

---
+++ src/layouts/MainLayout.jsx
@@ -0,0 +1,12 @@
+import NavBar from "../components/NavBar";
+import { Outlet } from "react-router-dom";
+const MainLayout = () => {
+  return (
+    <>
+      <NavBar />
+      <Outlet />
+    </>
+  );
+};
+
+export default MainLayout;
